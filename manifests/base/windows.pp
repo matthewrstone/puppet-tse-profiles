f1fe415a11a2cbf304dfc733db9fae04d6e00750
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-05-26 05:12:16 -0700

    winders

--- manifests/base/windows.pp
+++ manifests/base/windows.pp
@@ -4,4 +4,16 @@ class profile::base::windows(){
     dsc_name => $::build_hostname
   }
   reboot { 'dsc_reboot' : when => pending }
+  $hostfile = $kernel ? {
+    'Linux'   => '/etc/hosts',
+    'windows' => 'c:\\windows\\system32\\drivers\\etc\\hosts',
+  }
+  @@host { $::fqdn :
+    ensure       => present,
+    comment      => 'Managed by Puppet',
+    host_aliases => $::hostname,
+    ip           => $::ipaddress,
+    target       => $hostfile
+  }
+  Host <<| |>>
 }
