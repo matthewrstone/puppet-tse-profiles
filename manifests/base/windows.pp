10909706654b68b09309a5d326734f15a3b47e3a
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-05-26 04:53:33 -0700

    pdk convert

--- manifests/base/windows.pp
+++ manifests/base/windows.pp
@@ -1,1 +1,7 @@
-class profile::base::windows(){}
+# Windows Base Profile
+class profile::base::windows(){
+  dsc_xcomputer { $::certname :
+    dsc_name => $::build_hostname
+  }
+  reboot { 'dsc_reboot' : when => pending }
+}
