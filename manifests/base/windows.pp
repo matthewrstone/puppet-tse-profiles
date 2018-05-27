d19b3ca9571eafed4ae499c147d13b63265c516e
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-05-27 06:17:43 -0700

    Update title

--- manifests/base/windows.pp
+++ manifests/base/windows.pp
@@ -1,6 +1,6 @@
 # Windows Base Profile
 class profile::base::windows(){
-  dsc_xcomputer { $::certname :
+  dsc_xcomputer { $::hostname :
     dsc_name => $trusted['extensions']['pp_hostname']
   }
   reboot { 'dsc_reboot' : when => pending }
