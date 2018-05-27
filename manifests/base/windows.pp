0aeaf3c11735f3faea462e982168842ac88aa40d
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-05-27 06:44:44 -0700

    upad with trusted names

--- manifests/base/windows.pp
+++ manifests/base/windows.pp
@@ -1,17 +1,20 @@
 # Windows Base Profile
 class profile::base::windows(){
+  $build_domain     = $trusted['extensions']['pp_hostname']
+  $build_hostname   = $trusted['extensions']['pp_hostname']
+  $build_fqdn       = "${build_hostname}.${build_domain}"
   dsc_xcomputer { 'Rename Computer' :
-    dsc_name => $trusted['extensions']['pp_hostname']
+    dsc_name => $build_hostname
   }
   reboot { 'dsc_reboot' : when => pending }
   $hostfile = $kernel ? {
     'Linux'   => '/etc/hosts',
     'windows' => 'c:\\windows\\system32\\drivers\\etc\\hosts',
   }
-  @@host { $::fqdn :
+  @@host { $build_hostname :
     ensure       => present,
     comment      => 'Managed by Puppet',
-    host_aliases => $::hostname,
+    host_aliases => $build_fqdn,
     ip           => $::ipaddress,
     target       => $hostfile
   }
