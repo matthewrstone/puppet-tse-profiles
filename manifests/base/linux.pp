19276d05b77d36cf00d348f8a6668e0cb6532aaa
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-06-09 06:32:30 -0700

    manage hosts

--- manifests/base/linux.pp
+++ manifests/base/linux.pp
@@ -6,4 +6,15 @@ class profile::base::linux(){
   ].each | $pkg | {
     package { $pkg : ensure => present }
   }
+  $hostfile = $kernel ? {
+    'Linux'   => '/etc/hosts',
+    'windows' => 'c:\\windows\\system32\\drivers\\etc\\hosts',
+  }
+  @@host { $::fqdn :
+    ensure       => present,
+    comment      => 'Managed by Puppet',
+    host_aliases => $::hostname,
+    ip           => $::ipaddress,
+  }
+  Host <<| |>>
 }
