e855b893ecf04c68e5df700de9a3f7eb9ac89c6d
Matthew Stone <matthew.stone@matthew.stone-C02Q55TNFVH8>
2018-06-11 06:53:16 -0700

    fix the base

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,4 @@
 class profile::base{
-  $build_domain     = $trusted['domain']
-  $build_hostname   = $trusted['hostname']
-  $build_fqdn       = "${build_hostname}.mrs.puppetdemo.net"
   $hostfile = $kernel ? {
     'Linux'   => '/etc/hosts',
     'windows' => 'c:\\windows\\system32\\drivers\\etc\\hosts',
@@ -9,7 +6,7 @@ class profile::base{
   @@host { $::hostname :
     ensure       => present,
     comment      => 'Managed by Puppet',
-    host_aliases => $build_fqdn,
+    host_aliases => "${::hostname}.mrs.puppetdemo.net",
     ip           => $::ipaddress,
     target       => $hostfile
   }
